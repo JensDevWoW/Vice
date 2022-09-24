a58741f0fd1224ad1dd600e7d0be3832d6a3070a
Jens <jordan83222@gmail.com>
2022-09-24 10:27:28 -0400

    Implement UpdateListsMaterial function

--- defs.h
+++ defs.h
@@ -57,9 +57,10 @@ typedef struct {
     U64 posKey;
 
     int pceNum[13];
-    int bigPce[3];
-    int majPce[3];
-    int minPce[3];
+    int bigPce[2];
+    int majPce[2];
+    int minPce[2];
+    int material[2];
 
     S_UNDO history[MAXGAMEMOVES];
 
@@ -90,6 +91,13 @@ extern char PceChar[];
 extern char SideChar[];
 extern char RankChar[];
 extern char FileChar[];
+
+extern int PieceBig[13];
+extern int PieceMaj[13];
+extern int PieceMin[13];
+extern int PieceVal[13];
+extern int PieceCol[13];
+
 /* FUNCTIONS */
 
 // init.c
@@ -106,5 +114,5 @@ extern U64 GeneratePosKey(const S_BOARD *pos);
 extern void ResetBoard(S_BOARD *pos);
 extern void PrintBoard(const S_BOARD *pos);
 extern int ParseFen(char *fen, S_BOARD *pos);
-
+extern void UpdateListsMaterial(S_BOARD *pos);
 #endif

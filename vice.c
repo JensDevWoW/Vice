97f636e0f2a85455055c8ee1d472f88dff9f18f3
Jens <jordan83222@gmail.com>
2022-09-24 14:52:35 -0400

    N/A

--- vice.c
+++ vice.c
@@ -1,9 +1,34 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "defs.h"
+// vice.c
 
-int main()
-{
-    AllInit();
-    return 0;
+#include "stdio.h"
+#include "defs.h"
+#include "stdlib.h"
+
+#define PAWNMOVES "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+
+
+int main() {	
+
+	AllInit();		
+	
+	S_BOARD board[1];
+	
+	ParseFen(PAWNMOVES,board);
+	PrintBoard(board);
+	
+	S_MOVELIST list[1];
+	
+	GenerateAllMoves(board,list);
+	
+	PrintMoveList(list);
+	
+	return 0;
 }
+
+
+
+
+
+
+
+
